44791192a9ed0712c9ee23bd98f9a2d81d70e267
Abdelmjid Saber <abdelmjid999@gmail.com>
2026-06-17 20:58:12 +0300

    use action/method in Register form

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -6,7 +6,6 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Spinner } from '@/components/ui/spinner';
-import { store } from '@/actions/Laravel/Fortify/Http/Controllers/RegisteredUserController';
 import { login } from '@/routes';
 
 export default function Register() {
@@ -14,7 +13,8 @@ export default function Register() {
         <>
             <Head title="Register" />
             <Form
-                {...store.form()}
+                action="/register"
+                method="post"
                 resetOnSuccess={['password', 'password_confirmation']}
                 disableWhileProcessing
                 className="flex flex-col gap-6"
